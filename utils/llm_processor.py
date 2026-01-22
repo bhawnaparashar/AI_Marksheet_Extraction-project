e8fa118d3e9777fd15f3cc5d4b123505c6d7955e
bhawnaparashar <parasharbhawna51@gmail.com>
2026-01-22 18:20:56 +0530

    Add LLm Processor

---
+++ utils/llm_processor.py
@@ -0,0 +1,32 @@
+import google.generativeai as genai
+import os, json
+
+genai.configure(api_key=os.getenv("AIzaSyAVsDQOAOD2SXzFGTnMV7djqPg_ZOvS40k"))
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+async def process_text_with_llm(text):
+    prompt = f"""
+Extract marksheet information and return STRICT JSON.
+
+Fields:
+- name
+- father_name
+- roll_no
+- subject_wise_marks
+- overall_result
+
+Each field must contain:
+- value
+- confidence (0–1)
+
+TEXT:
+{text}
+"""
+
+    response = model.generate_content(prompt)
+    output = response.text.strip()
+
+    try:
+        return json.loads(output)
+    except:
+        return {"raw_output": output}
